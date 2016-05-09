9bc9c7e2862f2cc4815d5b2f76e9225b16b96c13
HelloBoxOffice <cacrawford92@gmail.com>
2016-05-09 23:46:10 +0100

    player controller and collisions added

---
+++ Assets/scripts/game/destroyOnParticleEnd.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destroyOnParticleEnd : MonoBehaviour {
+	public ParticleSystem ps;
+
+	void Start() {
+
+		ps = gameObject.GetComponent<ParticleSystem>();
+	}
+
+	void Update() {
+
+		if (ps) {
+			if (!ps.IsAlive ()) {
+				Destroy (gameObject);
+			}
+		}
+
+	}
+
+}
---
+++ Assets/scripts/player/playerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 10.0f;
+	public GameObject bullet;
+
+	public ParticleSystem explosion;
+
+	void Update() {
+
+
+		if (gameObject.tag == "Player") {
+			//player movement
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical"); 
+			transform.position = transform.position + new Vector3 (horizontal, vertical, 0) * (speed * Time.deltaTime);
+		} else {
+			return;
+		}
+
+
+	}
+
+
+	//runs particle effect when killed
+	void OnTriggerEnter () {
+		Instantiate (explosion, this.transform.position,Quaternion.identity);
+		Destroy (gameObject);
+	}
+
+}
